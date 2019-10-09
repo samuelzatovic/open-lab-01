14f38e8551620a7310ebcb88a19ccf76190f8083
samuel zaťovič <szatovic@gmail.com>
2019-10-09 17:21:48 +0200

    Hello Xamarin Lab

---
+++ open-lab-01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace open_lab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Xamarin Lab!");
+        }
+    }
+}
